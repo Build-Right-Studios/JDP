9c89464b41adc5a4c17cfa066318cc0c10823b89
Aryesh02 <aryeshsrivastava@gmail.com>
2025-08-28 02:53:42 +0530

    Categories Section done

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from '../components/Hero'
+import Categories from '../components/Categories'
+
+function Home() {
+    return (
+        <div>
+            <HeroSection />
+            <Categories />
+        </div>
+    )
+}
+
+export default Home
