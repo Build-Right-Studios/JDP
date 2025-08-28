e904af2a1516708823ef772174856028fadca37c
Soumilmittal <mittasoumill@gmail.com>
2025-08-29 01:28:29 +0530

    minute change

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -8,13 +8,13 @@ const WelcomeSection = () => {
         <h1 className="text-3xl sm:text-4xl font-bold tracking-widest uppercase mb-6 text-stone-900">
           WELCOME!
         </h1>
-        
+
         <div className="border border-stone-500/80 px-6 py-8 sm:px-10 sm:py-10 mb-8">
           <p className="text-base sm:text-lg leading-relaxed text-justify">
             Pumpernickel + Rye is a down-to-earth, homegrown market and new school deli with a modern vibe where our goal is to fulfill your body + soul cravings. As sisters and the best of friends, for us deli means family – and we invite you to our table for a nosh! Indulge yourself in the flavors that inspired our love for deli creations, as well as some elevated culinary finds from our travels around the world.
           </p>
         </div>
-        
+
         <p className="text-base sm:text-lg leading-relaxed text-justify mb-12">
           You'll be obsessed with our smoothie bar and our locally-sourced espressos + coffee will get you through the daily grind. Pick up a pound of our responsibly sourced, house-smoked meats and cured fish, try one of our daily soup specials, or come chill with us at our laid back yet vibrant restored farmhouse in Urbana.
         </p>
