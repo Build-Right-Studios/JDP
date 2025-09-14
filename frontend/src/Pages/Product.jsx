a72607459e840d40dff7ab758e0ae516d97f546b
Soumilmittal <mittasoumill@gmail.com>
2025-09-14 21:47:45 +0530

    added content in product page under pages

---
+++ frontend/src/Pages/Product.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import OilPage from './OilPage';
+
+
+function Product() {
+  return (
+    <div>
+      <Hero name= "PRODUCTS"></Hero>
+        <GreenBar></GreenBar>
+        <OilPage></OilPage>
+        
+    </div>
+  )
+}
+
+export default Product;
