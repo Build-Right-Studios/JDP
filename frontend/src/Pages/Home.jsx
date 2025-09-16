bea521c719ad3ab84ed4f786c5cde93e18e7d3be
Soumilmittal <mittasoumill@gmail.com>
2025-09-17 02:12:58 +0530

    final made

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,26 +1,23 @@
 import React from 'react'
 // import HeroSection from '../components/Hero'
-import Categories from '../components/Home/Categories'
+import Categories from '../components/Home/Categories';
 // import Welcome from '../components/Welcome'
 // import HumareKhass from '../components/HumareKhass'
-import Hero from '../components/AboutUs/hero'
-import GreenBar from '../components/AboutUs/GreenBar'
-import BestSeller from '../components/Home/BestSeller'
-import OurStore from '../components/Home/OurStore'
-import Comments from '../components/Home/Comments'
-import bannerImage from '../../images/homebanner.png';
+import GreenBar from '../components/AboutUs/GreenBar';
+import BestSeller from '../components/Home/BestSeller';
+import OurStore from '../components/Home/OurStore';
+import Comments from '../components/Home/Comments';
+import bannerImage from '../images/homebanner.png';
 
 function Home() {
     return (
         <div>
-            <div className="w-full"> 
+            <div className="w-full h-[90vh]"> 
                 <img 
                     src={bannerImage} 
-                    alt="Home Banner" 
-                    className="w-full h-auto object-cover" 
+                    className="w-full h-[90vh] object-cover" 
                 />
             </div>
-
             <GreenBar />
             <Categories />
             <OurStore />
