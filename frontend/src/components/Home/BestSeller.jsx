0b63e711be221ea07836b5674df24fa758649a15
Aryesh02 <aryeshsrivastava@gmail.com>
2025-09-16 21:42:16 +0530

    Changes to Best Seller

--- frontend/src/components/Home/BestSeller.jsx
+++ frontend/src/components/Home/BestSeller.jsx
@@ -4,7 +4,7 @@ import img1 from '../../images/oil.png'
 
 function BestSeller() {
     return (
-        <div className='bg-gray-100 mt-2'>
+        <div className='mt-20'>
             <h2 className="text-5xl font-extrabold text-center mt-8 mb-8 font-['Poppins']">BEST SELLERS</h2>
             <div className='grid grid-cols-1 lg:grid-cols-3 px-2 mx-auto place-items-center gap-y-2'>
                 <BestSellerCard color = "#b45309" title = "SUN FLOWER OIL" image={img1} description="Enjoy the goodness of our freshly produced, cold-pressed Sunflower Oil—packed with natural nutrients and authentic flavor. 100% chemical-free, it’s the healthy choice for your everyday cooking."/>
