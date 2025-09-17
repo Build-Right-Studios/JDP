2825520e0abbd9434c87cb164ba371252fd79812
Aryesh02 <aryeshsrivastava@gmail.com>
2025-09-17 13:00:22 +0530

    Changes to Video file 2.0

--- frontend/src/components/Home/OurStore.jsx
+++ frontend/src/components/Home/OurStore.jsx
@@ -23,7 +23,7 @@ function OurStore() {
             className="w-full max-h-[60vh] lg:max-h-[80vh] object-contain"
             playsInline
           >
-            <source src={videoFile} type="video/mp4" />
+            <source src="/Slider/pure1.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
